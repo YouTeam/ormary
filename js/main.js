3d9e35afad9cc699261c44b266f64ab6db846ce9
OlegShpaner <oleg.shpaner@qubstudio.com>
2014-05-15 19:23:32 +0300

    Added new page (product)

--- js/main.js
+++ js/main.js
@@ -27,3 +27,45 @@ $(document).ready(function(){
 		$('.filter-clothing-womens').hide();
 	});
 });
+
+/* Main slider */
+
+var mySwiper = new Swiper('.orm_main_slider',{
+  pagination: '.pagination',
+  paginationClickable: true,
+  loop: true,
+  autoplay:3000
+})
+$('.arrow-left').on('click', function(e){
+	e.preventDefault()
+	mySwiper.swipePrev()
+})
+$('.arrow-right').on('click', function(e){
+	e.preventDefault()
+	mySwiper.swipeNext()
+})
+
+/* Product carousel */
+
+  var myCarousel = new Swiper('.orm_product_carousel',{
+    paginationClickable: true,
+    slidesPerView: 5
+  })
+
+  $('.orm_carousel_prev').on('click', function(e){
+	e.preventDefault()
+	myCarousel.swipePrev()
+})
+$('.orm_carousel_next').on('click', function(e){
+	e.preventDefault()
+	myCarousel.swipeNext()
+})
+
+$(document).ready(function(){
+	$('.orm_carousel_slide').click(function(){
+		$(this).addClass('active');
+		$(this).siblings().removeClass('active');
+		var imgsource = $(this).children().children().attr('src');
+		$('.product_image img').attr('src',imgsource);
+	});
+});
